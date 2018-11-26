199e86c53e385045ab952c8dcd28e4242f6a7da0
Sebastian Stanici <sxs1476@cca-ug04-111.cs.bham.ac.uk>
2018-11-26 20:05:38 +0000

    Connection now works!

--- src/ChristmasPartyCompany.java
+++ src/ChristmasPartyCompany.java
@@ -5,17 +5,18 @@ import javax.sql.*;
 public class ChristmasPartyCompany {
     public static void main(String[] args) {
         // Part 1: Set up definition of the database (SQL DDL statements)
-        System.setProperty("jdbc.drivers", "org.postgresql.Driver");
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException ex) {
-            System.out.println("Driver not found! ");
-        }
+        // System.setProperty("jdbc.drivers", "org.postgresql.Driver");
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException ex) {
+//            System.out.println("Driver not found! ");
+//            return;
+//        }
         System.out.println("PostgreSQL driver registered.");
 
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:postgresql://tinky-winky.cs.bham.ac.uk/mod-intro-databases/cslibrary", "sxs1476", "DBpasswordLOL");
+            conn = DriverManager.getConnection("jdbc:postgresql://mod-intro-databases/cslibrary", "sxs1476", "DBpasswordLOL");
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
